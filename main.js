ca7a15fbef2e01a514d20df8640e046a95d655af
ahmed <ahmed.mohammed.developer@gmail.com>
2023-12-04 17:24:10 +0300

    End Footer & Info

--- main.js
+++ main.js
@@ -39,3 +39,13 @@ links
 
 
 */
+
+let el = document.querySelector(".scroller");
+let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+
+
+window.addEventListener("scroll", () => {
+   const scrollTop = document.documentElement.scrollTop;
+  el.style.width = `${(scrollTop / height) * 100}%`;
+});
